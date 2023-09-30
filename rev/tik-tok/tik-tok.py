75f904970f0d6e72146b686b28a5eeca61a587c4
Benson Liu <bensonhliu@gmail.com>
2023-09-30 08:46:05 -0700

    feat: created `tik-tok`

---
+++ rev/tik-tok/tik-tok.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    flag = input("Flag: ")
+    if ord(flag[27]) == 98 and ord(flag[34]) == 95 and ord(flag[1]) == 121 and ord(flag[8]) == 107 and ord(flag[15]) == 110 and ord(flag[46]) == 115 and ord(flag[37]) == 114 and ord(flag[19]) == 101 and ord(flag[20]) == 95 and ord(flag[13]) == 95 and ord(flag[22]) == 108 and ord(flag[39]) == 121 and ord(flag[41]) == 100 and ord(flag[23]) == 111 and ord(flag[44]) == 116 and ord(flag[18]) == 104 and ord(flag[47]) == 116 and ord(flag[10]) == 116 and ord(flag[12]) == 107 and ord(flag[16]) == 95 and ord(flag[36]) == 97 and ord(flag[38]) == 116 and ord(flag[24]) == 99 and ord(flag[31]) == 116 and ord(flag[25]) == 107 and ord(flag[30]) == 95 and ord(flag[28]) == 117 and ord(flag[2]) == 98 and ord(flag[33]) == 101 and ord(flag[21]) == 99 and ord(flag[32]) == 104 and ord(flag[45]) == 95 and ord(flag[35]) == 112 and ord(flag[17]) == 116 and ord(flag[26]) == 95 and ord(flag[14]) == 111 and ord(flag[0]) == 99 and ord(flag[11]) == 111 and ord(flag[40]) == 95 and ord(flag[6]) == 116 and ord(flag[7]) == 105 and ord(flag[49]) == 112 and ord(flag[29]) == 116 and ord(flag[42]) == 111 and ord(flag[48]) == 111 and ord(flag[4]) == 114 and ord(flag[9]) == 95 and ord(flag[5]) == 123 and ord(flag[3]) == 101 and ord(flag[43]) == 110 and ord(flag[50]) == 125:
+        print("Correct!")
+    else:
+        print("Incorrect!")
