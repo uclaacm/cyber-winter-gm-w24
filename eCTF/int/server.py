45fcb76b5cf9ac7bf0d6470a9c2ef622a50275fb
freed2003 <gary.song2003@gmail.com>
2023-11-28 14:43:21 -0800

    new challs

---
+++ eCTF/int/server.py
@@ -0,0 +1,14 @@
+from Crypto.Cipher import AES
+import os
+
+key = os.urandom(16)
+flag = b'\xfa\x07\x82aG\xaci\x1c\x9d+`\x93eSR]\xe0\xf6\xf4\xf1T\x04|A\xea\xbd\xed\xa5\xad\x01;\x99'
+cipher = AES.new(key, AES.MODE_ECB)
+
+while True:
+    todecrypt = input("What should I decrypt? Enter as hex: ")
+    todecrypt = bytes.fromhex(todecrypt)
+    if todecrypt == flag:
+        print("I won't decrypt the flag silly, try again!")
+    else:
+        print(cipher.decrypt(todecrypt))
